976110083eefe41744c851ddd9c99e61021f29ea
Andrew Schalk <42019020+andrewschalk@users.noreply.github.com>
2024-01-11 23:25:08 -0600

    Update build.js

--- build.js
+++ build.js
@@ -15,17 +15,18 @@ const options = {
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('                    Andrew Schalk'),
-  description: chalk.white('Robotics Engineering Student at SIUE'),
+  name: chalk.white('                      Andrew Schalk'),
+  description: chalk.white('         Robotics Engineering Student at ')+ chalk.red('SIUE'),
   npm: chalk.gray('https://npmjs.com/') + chalk.red('~andrewschalk'),
   github: chalk.gray('https://github.com/') + chalk.green('andrewschalk'),
   linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('andrew-schalk-999550285'),
-  web: chalk.cyan('aschalk@siue.edu'),
-  labelDescription: chalk.white.bold('    Description:'),
+  card: chalk.gray('npx ') + chalk.yellow('andrewtheengineer'),
+  email: chalk.cyan('aschalk@siue.edu'),
+  labelDescription: chalk.white.bold(''),
   labelnpm: chalk.white.bold('     npm:'),
   labelGitHub: chalk.white.bold('  GitHub:'),
   labelLinkedIn: chalk.white.bold('LinkedIn:'),
-  labelWeb: chalk.white.bold('     Web:'),
+  labelEmail: chalk.white.bold('   Email:'),
   labelCard: chalk.white.bold('    Card:')
 }
 
@@ -36,17 +37,17 @@ const working = `${data.labelDescription}  ${data.description}`
 const npming = `${data.labelnpm}  ${data.npm}`
 const githubing = `${data.labelGitHub}  ${data.github}`
 const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
-const webing = `${data.labelWeb}  ${data.web}`
-const carding = `${data.labelCard}  ${data.npx}`
+const emailing = `${data.labelEmail}  ${data.email}`
+const carding = `${data.labelCard}  ${data.card}`
 
 // Put all our output together into a single variable so we can use boxen effectively
-const output = heading + // data.name + data.handle
-               newline + newline + // Add one whole blank line
+const output = newline + newline + heading + newline +// data.name + data.handle
                working + newline + // data.labelDescription + data.description
-               npming + newline + // data.labelnpm + data.npm
-               githubing + newline + // data.labelGitHub + data.github
+			   newline + // Add one whole blank line
+               githubing + newline + // data.labelnpm + data.npm
+               npming + newline + // data.labelGitHub + data.github
                linkedining + newline + // data.labelLinkedIn + data.linkedin
-               webing + newline + newline + // data.labelWeb + data.web
-               carding // data.labelCard + data.npx
+               emailing + newline + newline + // data.labelEmail + data.email
+               carding +newline + newline // data.labelCard + data.npx
 
-fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
+fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.blue(boxen(output, options)))
